cfd8274b99654d9db4e805541abbf920035386ca
Marcel Solle <msap3rtur3@gmail.com>
2019-06-14 09:23:06 +0200

    Moved index.js into src folder

--- index.js
+++
@@ -1,4 +0,0 @@
-import VueEventRegistry from './src/vue-plugin';
-
-export default VueEventRegistry
-export { createEventRegistry } from './src/event-registry';
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import VueEventRegistry from './vue-plugin';
+
+export default VueEventRegistry
+export { createEventRegistry } from './event-registry';
